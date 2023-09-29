24c4b4249df6b8a1b27b94a24194f9bbb6e1b565
= <=>
2023-09-29 20:57:28 +0700

    fix conflict

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
-=======
-import path_provider_foundation
->>>>>>> b0d5d4b466d59e5fb3a18cd0ced3fac73b33d596
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
 }
