8c164781f5b363b69a7c661ac4968a6ee8b6a6e7
f0ursecond <2ez4second@gmail.com>
2023-10-04 10:45:23 +0700

    feat: change border & add refresh indicator

--- lib/features/prayer/prayer_screen.dart
+++ lib/features/prayer/prayer_screen.dart
@@ -10,94 +10,108 @@ class PrayerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final cubit = PrayerCubit()..getPrayerList();
+    final cubit = BlocProvider.of<PrayerCubit>(context);
     final searchController = TextEditingController();
     final search = _SearchCubit();
 
     return Scaffold(
       appBar: AppBar(title: const Text('Doa Doa Harian')),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 10),
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: searchController,
-                  textInputAction: TextInputAction.search,
-                  onChanged: search.search,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Search',
-                    suffixIcon: Align(
-                      heightFactor: 1.0,
-                      widthFactor: 1.0,
-                      child: Icon(EvaIcons.search_outline),
+        child: RefreshIndicator(
+          onRefresh: () async {
+            await context.read<PrayerCubit>().getPrayerList();
+          },
+          child: SingleChildScrollView(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 10),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: searchController,
+                    textInputAction: TextInputAction.search,
+                    onChanged: search.search,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Search',
+                      suffixIcon: Align(
+                        heightFactor: 1.0,
+                        widthFactor: 1.0,
+                        child: Icon(EvaIcons.search_outline),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                BlocBuilder<PrayerCubit, PrayerState>(
-                  bloc: cubit,
-                  builder: (context, state) {
-                    if (state is PrayerListSucces) {
-                      return BlocBuilder<_SearchCubit, String>(
-                        bloc: search,
-                        builder: (context, query) {
-                          var raw = state.listPrayer;
-                          var filteredList = raw
-                              .where((element) => element.title!
-                                  .toLowerCase()
-                                  .contains(query.toLowerCase()))
-                              .toList();
-                          if (filteredList.isNotEmpty) {
-                            return ListView.separated(
-                              separatorBuilder: (context, index) {
-                                return const SizedBox(height: 10);
-                              },
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemCount: filteredList.length,
-                              itemBuilder: (context, index) {
-                                var data = filteredList[index];
-                                return Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey.shade400,
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  child: Theme(
-                                    data: ThemeData().copyWith(
-                                      dividerColor: Colors.transparent,
-                                    ),
-                                    child: ExpansionTile(
-                                      title: Text(
-                                        data.title ?? '',
-                                        style: const TextStyle(
-                                            color: Colors.black),
+                  const SizedBox(height: 10),
+                  BlocProvider.value(
+                    value: cubit..getPrayerList(),
+                    child: BlocBuilder<PrayerCubit, PrayerState>(
+                      builder: (context, state) {
+                        if (state is PrayerListSucces) {
+                          return BlocBuilder<_SearchCubit, String>(
+                            bloc: search,
+                            builder: (context, query) {
+                              var raw = state.listPrayer;
+                              var filteredList = raw
+                                  .where((element) => element.title!
+                                      .toLowerCase()
+                                      .contains(query.toLowerCase()))
+                                  .toList();
+                              if (filteredList.isNotEmpty) {
+                                return ListView.separated(
+                                  separatorBuilder: (context, index) {
+                                    return const SizedBox(height: 10);
+                                  },
+                                  shrinkWrap: true,
+                                  physics: const BouncingScrollPhysics(),
+                                  itemCount: filteredList.length,
+                                  itemBuilder: (context, index) {
+                                    var data = filteredList[index];
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        border: Border.all(
+                                          color: Colors.black,
+                                        ),
+                                        borderRadius: BorderRadius.circular(8),
                                       ),
-                                      children: [
-                                        ExpansionTileContent(data: data)
-                                      ],
-                                    ),
-                                  ),
+                                      child: Theme(
+                                        data: ThemeData().copyWith(
+                                          dividerColor: Colors.transparent,
+                                        ),
+                                        child: ExpansionTile(
+                                          title: Text(
+                                            data.title ?? '',
+                                            style: const TextStyle(
+                                                color: Colors.black),
+                                          ),
+                                          children: [
+                                            ExpansionTileContent(data: data)
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  },
                                 );
-                              },
-                            );
-                          } else {
-                            return const Center(
-                              child: Text('Hasil Pencarian Tidak Ditemukan'),
-                            );
-                          }
-                        },
-                      );
-                    } else if (state is PrayerListFailure) {
-                      return Text(state.message);
-                    } else {
-                      return const CustomShimmerLoading();
-                    }
-                  },
-                ),
-              ],
+                              } else {
+                                return const Center(
+                                  child:
+                                      Text('Hasil Pencarian Tidak Ditemukan'),
+                                );
+                              }
+                            },
+                          );
+                        } else if (state is PrayerListFailure) {
+                          return Text(state.message);
+                        } else if (state is PrayerListLoading) {
+                          return const CustomShimmerLoading();
+                        } else {
+                          return const CustomShimmerLoading();
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
